fb470e839d7b2810af4000ebc506a390681ce985
deckelmouck <nikolaspalm@gmail.com>
2019-05-17 12:44:00 +0200

    save

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace test_hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string input = Console.ReadLine();
+
+            string output = getHash(input);
+            
+            Console.WriteLine($"{input}: {output}");
+        }
+
+        private static string getHash(string text)
+        {
+            using(var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
