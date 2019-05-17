8132df694a7244061fa5ada1e2fd5f740bdc031e
deckelmouck <nikolaspalm@gmail.com>
2019-05-17 12:45:29 +0200

    save

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -9,21 +11,81 @@ namespace test_hash
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            string input = Console.ReadLine();
-
-            string output = getHash(input);
-            
-            Console.WriteLine($"{input}: {output}");
+            string text2hash = Console.ReadLine();
+            tryIt(text2hash);
+            Console.ReadLine();
         }
 
-        private static string getHash(string text)
+        private static string getHash(string text, string salt)
         {
-            using(var sha256 = SHA256.Create())
+            using (var sha256 = SHA256.Create())
             {
-                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
-                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+                string toHash = string.Concat(text, salt);
+
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(toHash));
+
+                var hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                return hash;
             }
+        }
+
+        private static string getSalt()
+        {
+            byte[] bytes = new byte[128 / 8];
+            using (var keyGenerator = RandomNumberGenerator.Create())
+            {
+                keyGenerator.GetBytes(bytes);
+                return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+            }
+        }
+
+        private static void tryIt(string text)
+        {
+            string saltSolution = "";
+            string puzzleStart = "ab";
+            int count = 1;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            TimeSpan timeSpan = new TimeSpan();
+
+            List<string> listUsedSalts = new List<string>();
+
+            while (true)
+            {
+                saltSolution = getSalt();
+
+                //if (!listUsedSalts.Contains(saltSolution))
+                //{
+                    string hashSolution = getHash(text, saltSolution);
+                    //listUsedSalts.Add(saltSolution);
+
+                    if (hashSolution.Substring(0, 2) == puzzleStart)
+                    {
+                        timeSpan = watch.Elapsed;
+                        Console.WriteLine($"Solution found! - Seconds needed: {timeSpan.TotalSeconds.ToString()}");
+                        //Console.WriteLine($"text: {text} - salt: {saltSolution} - hash: {hashSolution}");
+                        Console.WriteLine($"try {count.ToString()} - hash: {hashSolution}");
+                    break;
+                    }
+                    else if (count > 10000000)
+                    {
+                        timeSpan = watch.Elapsed;
+                        Console.WriteLine($"Soluton not found! Seconds needed: {timeSpan.TotalSeconds.ToString()}");
+                        break;
+                    }
+                    else if ((count % 100000) == 0)
+                    {
+                        timeSpan = watch.Elapsed;
+                        Console.Clear();
+                        Console.WriteLine($"Try: {count} - Seconds running: {timeSpan.TotalSeconds.ToString()}");
+                    }
+                    //count++;
+                //}
+                count++;
+            }
+
+            watch.Stop();
         }
     }
 }
